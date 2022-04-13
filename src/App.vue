948bb85561a443aa177028666c5b267bbe080a50
javier <javier.felix@gcabsa.com.mx>
2022-04-12 17:27:30 -0700

    Se creó el archivo header
    component

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,11 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
 import HelloWorld from './components/HelloWorld.vue'
+import TheHeader from './components/TheHeader.vue'
 </script>
 
 <template>
+  <TheHeader />
   <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Hello Vue 3 + Vite" />
 </template>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,5 @@
+<template>
+    <header>
+        <h1>Este es el header</h1>
+    </header>
+</template>
